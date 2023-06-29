9d251b544014f20fb12cd69fa0be5d9fb31e84e6
maleficsydney <maleficsydney@gmail.com>
2023-06-29 17:46:59 -0500

    isecondary commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
-// import { useState } from 'react'
+import { useState } from "react";
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 // import './App.css'
+import { puppyList } from "./data.js";
 
 function App() {
-  // const [count, setCount] = useState(0)
+  const [puppies, setPuppies] = useState(puppyList);
 
   return (
     <>
-      <div>
-       
+      <div className="App">
+        {puppies.map((puppy) => {
+          return <p key={puppy.id}> {puppy.name} </p>; // React requires a key to insure the integrity of the order of elements, why?
+        })}
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

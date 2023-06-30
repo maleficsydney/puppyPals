0e38075625316d81562907ccac2d2c7f2cb492f9
maleficsydney <maleficsydney@gmail.com>
2023-06-29 19:09:47 -0500

    third commit - will format with css later

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,48 @@
 import { useState } from "react";
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
+import "./App.css";
 import { puppyList } from "./data.js";
+// import { isCSSRequest } from "vite";
 
 function App() {
-  const [puppies, setPuppies] = useState(puppyList);
+  const [puppies] = useState(puppyList); // the workshop never reads setPuppies?
+  const [featPupId, setFeatPupId] = useState(null);
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+  console.log(featuredPup);
+
+  let isCute =
+    featuredPup &&
+    featuredPup.isCute && // If this is true, I want you to do everything that comes afterwards
+    "This puppy is cute!";
 
   return (
     <>
       <div className="App">
         {puppies.map((puppy) => {
-          return <p key={puppy.id}> {puppy.name} </p>; // React requires a key to insure the integrity of the order of elements, why?
+          return (
+            <p
+              onClick={() => {
+                setFeatPupId(puppy.id);
+              }}
+              key={puppy.id}
+            >
+              {" "}
+              {puppy.name}{" "}
+            </p>
+          ); // React requires a key to insure the integrity of the order of elements, why? - A specific
+          // tracking the ID via UseState? laywoman's terms pls
         })}
       </div>
+
+      {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <p>{featuredPup.email}</p>
+            <p>{featuredPup.age}</p>
+            <p>{isCute}</p>
+          </ul>
+        </div>
+      )}
     </>
   );
 }
